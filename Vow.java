bdbcd8d4cf4f643f9796f3b150fb4688e96c14c1
preethilatha <preethilatha08@gmail.com>
2016-08-30 12:38:48 +0530

    Create Vow.java

---
+++ Vow.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Vow
+{
+public static void main(string[] args)
+{
+char ch;
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+System.out.println("it is vowel");
+}
+else
+{
+System.out.println("it is constants");
+}}}
