736d84192741d9c7dac9eee89bd262f137c48abf
preethilatha <preethilatha08@gmail.com>
2016-08-30 12:22:07 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Odd
+{
+public static void main(String[] args)
+{
+int n=8;
+if(n%2==0)
+{
+System.out.println("it is odd");
+}
+else
+{
+System.out.println("it is even");
+}
+}
+}
