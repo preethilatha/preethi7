e919205459cb6bbc0f4c19f96a59e1c748d97c9e
preethilatha <preethilatha08@gmail.com>
2016-08-30 12:28:20 +0530

    Create Pos.java

---
+++ Pos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Pos
+{
+public static void main(string[] args)
+{
+int n=6;
+if(n<0)
+{
+System.out.println("the given is negative");
+}
+else if(n>0)
+{
+System.out.println("the given is positive");
+}
+else
+{System.out.println("the given is zero");
+}}}
