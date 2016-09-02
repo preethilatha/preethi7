160e3a1e69e17bec9156e62aadce1d83509d37a8
preethilatha <preethilatha08@gmail.com>
2016-09-02 14:05:35 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Largest
+{
+int a=23,b=34,c=15;
+if(a>c)
+{
+else if(a>b)
+{
+System.out.print("a is largest");
+}
+else if(b>c){
+System.out.print("b is largest");
+}
+else
+{
+System.out.print("c is largest");
+}}}
