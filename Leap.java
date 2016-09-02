cd38c50703b455d1ac38e3e80082c2a073a1550d
preethilatha <preethilatha08@gmail.com>
2016-09-02 14:11:01 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Leap
+{
+int n;
+n=integer.parseInt(String[] args);
+System.out.print("enter the year");
+if(n%4==0)
+{
+System.out.print("leap year");
+}
+else
+{
+System.out.print("non-leap year");
+}}}
